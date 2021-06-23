1b67a9e7622147119a8056e2b84b66bcbe1d955e
Karunika <karunikatrip@gmail.com>
2021-06-23 19:53:11 +0200

    Fixed Canvas Image

--- src/include/drawBox.ts
+++ src/include/drawBox.ts
@@ -1,3 +1,32 @@
+// interface IHash {
+//     [details: string] : any;
+// } 
+
+// const box: IHash = {
+//     "black" : require('../assets/black.png'),
+//     "blue" : require('../assets/blue.png'),
+//     "cyan" : require('../assets/cyan.png'),
+//     "green" : require('../assets/green.png'),
+//     "orange" : require('../assets/orange.png'),
+//     "purple" : require('../assets/purple.png'),
+//     "red" : require('../assets/red.png'),
+//     "yellow" : require('../assets/yellow.png')
+// }
+// let piece = new Image();
+
+// export default function drawBox(ctx: CanvasRenderingContext2D, x: number, y: number, size: number, color: string) : void{
+//     if(color == `black`){
+//         ctx.fillStyle = `rgb(26,26,26)`;
+//         ctx.fillRect(x*size, y*size, size, size);
+//         ctx.lineWidth = 0.1;
+//         ctx.strokeStyle = "white";
+//         ctx.strokeRect(x*size, y*size, size, size);
+//     }else{
+//         let c = box[color].replace(/^public/, "");
+//         piece.src = c;
+//         ctx.drawImage(piece, x*size, y*size, size, size);
+//     }
+// };
 interface IHash {
     [details: string] : any;
 } 
@@ -12,7 +41,12 @@ const box: IHash = {
     "red" : require('../assets/red.png'),
     "yellow" : require('../assets/yellow.png')
 }
-let piece = new Image();
+for(let c in box){
+    let boxImg = new Image();
+    box[c] = box[c].replace(/^public/, "")
+    boxImg.src = box[c];
+    box[c] = boxImg;
+}
 
 export default function drawBox(ctx: CanvasRenderingContext2D, x: number, y: number, size: number, color: string) : void{
     if(color == `black`){
@@ -22,8 +56,6 @@ export default function drawBox(ctx: CanvasRenderingContext2D, x: number, y: num
         ctx.strokeStyle = "white";
         ctx.strokeRect(x*size, y*size, size, size);
     }else{
-        let c = box[color].replace(/^public/, "");
-        piece.src = c;
-        ctx.drawImage(piece, x*size, y*size, size, size);
+        ctx.drawImage(box[color], x*size, y*size, size, size);
     }
 };
